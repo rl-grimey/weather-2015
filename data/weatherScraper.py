e10489765de7299eb6b7ef6adbf39e305e35dfeb
Ryan Larson <ryan-p-larson@uiowa.edu>
2015-12-26 22:40:36 -0600

    Working Scraper (until 2014)

--- data/weatherScraper.py
+++ data/weatherScraper.py
@@ -4,16 +4,30 @@ from bs4 import BeautifulSoup
 data = []
 
 def scrapeStation(station):
-    url = 'http://www.wunderground.com/history/airport/KIOW/{}/12/{}/DailyHistory.html'
-    formattedURL = url.format(1948, 1)
+    url = 'http://www.wunderground.com/history/airport/{}/{}/12/{}/DailyHistory.html'
+    day = range(1,32)
+    year = range(1948, 2015)
 
-    page = requests.get(formattedURL)
-    soup = BeautifulSoup(page.text, "html.parser")
-    table = soup.find(id='historyTable').find_all('span', class_='wx-value')
+    for i in year:
+        for j in day:
+            formattedURL = url.format(station, i, j)
+            page = requests.get(formattedURL)
+            soup = BeautifulSoup(page.text, "html.parser")
+            table = soup.find(id='historyTable').find_all('span', class_='wx-value')
 
-    meanTemp = table[0].text
-    maxTemp = table[1].text
-    minTemp = table[2].text
+            meanTemp = table[0].text
+            maxTemp = table[1].text
+            minTemp = table[2].text
 
+            if j == 31:
+                print ("year: "+str(j), meanTemp)
+            data.append({"year":i, "day":j, "meanTemp":meanTemp, "maxTemp":maxTemp, "minTemp":minTemp})
 
-scrapeStation("test")
+scrapeStation("KIOW")
+
+with open("december-scraped-IC.csv", "wb") as outfile:
+    attrNames = ["day", "year", "meanTemp", "maxTemp", "minTemp"]
+    writer = csv.DictWriter(outfile, fieldnames=attrNames)
+
+    writer.writeheader()
+    writer.writerows(data)
